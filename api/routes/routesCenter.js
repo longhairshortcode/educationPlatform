77adeb18018c16d842097cbafd6631af9f777f32
Rona <longhairshortcode@gmail.com>
2024-06-17 20:05:51 -0700

    changed controller to class methods and all should be updated

--- api/routes/routesCenter.js
+++ api/routes/routesCenter.js
@@ -1,21 +1,21 @@
 import express from "express"
-import auth from "./auth.route.js"
-import parent from "./parent.route.js" 
-import educator from "./educator.route.js" 
-import review from "./review.route.js" 
-import message from "./message.route.js" 
-import find from "./find.route.js" 
-import conference from "./conference.route.js" 
+import Auth from "./auth.route.js"
+import Parent from "./parent.route.js" 
+import Educator from "./educator.route.js" 
+import Review from "./review.route.js" 
+import Message from "./message.route.js" 
+import Find from "./find.route.js" 
+import Conference from "./conference.route.js" 
 
 const router = express.Router();
 
-router.use('/auth', auth)
-router.use('/parent', parent)
-router.use('/educator',educator)
-router.use('/review', review)
-router.use('/message', message)
-router.use('/find', find)
-router.use('/conference', conference)
+router.use('/auth', Auth)
+router.use('/parent', Parent)
+router.use('/educator',Educator)
+router.use('/review', Review)
+router.use('/message', Message)
+router.use('/find', Find)
+router.use('/conference', Conference)
 
 
 export default router 
