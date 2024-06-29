7df1018743d9ec6c46ee149f3748f2d10c678f5f
Rona <longhairshortcode@gmail.com>
2024-06-28 17:04:58 -0700

    started styling home page, did nav now started on hero

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -3,19 +3,24 @@ import style from "./Navbar.module.css"
 function Navbar() {
   return (
     <nav className={style.navContainer}>
-      <div className={style.leftLogoAndLinksContainer}>
-        <img alt="logo"/>
-        <div className={style.threeDDContainers}>
-            <p>Parents</p>
-            <p>Teachers</p>
-            <p>Games</p>
+      <div className={style.logoContainer}>
+        <div className={style.circlesContainer}>
+          <div className={style.circle1}></div>
+          <div className={style.circle2}></div>
         </div>
+        <p className={style.logoName}>EduTutor</p>
       </div>
-      <div className={style.righttLoginAndBuyContainer}>
-            <p>Log in</p>
-            <div className={style.buyButton}>
-                Buy Membership
-            </div>
+
+      <div className={style.linksContainer}>
+        <p className={`${style.links} ${style.about}`}>About</p>
+        <p className={`${style.links} ${style.tutoring}`}>Tutoring</p>
+        <p className={`${style.links} ${style.classes}`}>Classes</p>
+        <p className={`${style.links} ${style.educators}`}>Educators</p>
+        <p className={`${style.links} ${style.educators}`}>Pricing</p>
+        <p className={`${style.links} ${style.educators}`}>Contact Us</p>
+      </div>
+      <div className={style.signUpButtonContainer}>
+        <button className={style.signUpButton}>Sign Up</button>
       </div>
     </nav>
   )
