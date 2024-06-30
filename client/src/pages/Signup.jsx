a88d1967597e893377ca576e99f3306580a2f5c1
Rona <longhairshortcode@gmail.com>
2024-06-29 23:57:51 -0700

    styling home, parent and edu login, and signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -43,7 +43,7 @@ function Signup() {
               <input
                 type="password"
                 className={style.confirmPassword}
-                name="confirmPasswordsssssssssssd"
+                name="confirmPasswordssssssssssssd"
                 // value={userLogin.password}
                 // onChange={handleChange}
                 placeholder="Password"
