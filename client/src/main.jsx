6a722472fadc48731a825be66da23fe8df23452b
Rona <longhairshortcode@gmail.com>
2024-07-23 17:09:25 -0700

    working on getting All Tutors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+import {QueryClient, QueryClientProvider} from 'react-query'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
