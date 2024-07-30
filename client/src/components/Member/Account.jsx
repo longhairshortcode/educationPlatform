3dfd35792fc7c2fbb0a9b33742ce6bcee48bce52
Rona <longhairshortcode@gmail.com>
2024-07-30 14:25:15 -0700

    Almost done with member dashboard

--- client/src/components/Member/Account.jsx
+++ client/src/components/Member/Account.jsx
@@ -1,12 +1,37 @@
 import style from "./Account.module.css"
 
 function Account() {
-  return (
+    return (
       <div className={style.accountContainer}>
-        <div className={style.accountBox}>Messages</div>
-        <div className={style.accountBox}>Reviews</div>
-      </div>  
-  )
-}
-
-export default Account
+        <div className={style.dropdownContainerAndImage}>
+            <div className={style.dropdownContainer}>
+               <h1 className={style.hello}>Hello, Ms. Shine</h1>
+               <select id="dropdown" name="options" className={style.dropdown}>
+                    <option value="" disabled selected hidden className={style.profileOptions}>PROFILE OPTIONS</option>
+                    <option value="editProfile" className={style.editProfile}>EDIT PROFILE</option>
+                    <option value="viewProfile" className={style.viewProfile}>VIEW PROFILE</option>
+                </select>
+            </div>
+            <div className={style.profilePicContainer}>
+                <image className={style.profilePic}>PIC</image>
+            </div> 
+        </div>
+        <div className={style.accountSubBox}>
+            <div className={style.accountSubBoxLeft}>
+                <p className={`${style.paragraphs} ${style.manage}`}>MANAGE <br/> EDUCATOR ACCOUNT</p>
+            </div>
+            <div className={style.accountSubBoxRight}>
+                <p className={`${style.paragraphs} ${style.start}`}>START SESSION</p>
+            </div>
+        </div>
+        <div className={style.accountBox}>
+            <p className={style.paragraphs}>MESSAGES</p>
+        </div>
+        <div className={`${style.accountBox} ${style.reviewsBox}`}>
+            <p className={style.paragraphs}>REVIEWS</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Account;
