aadc6bead8e72ee36493b6211f6d333e978861ba
Rona <longhairshortcode@gmail.com>
2024-07-30 16:38:14 -0700

    Almost done with member dashboard

--- client/src/layout/NavMember.jsx
+++ client/src/layout/NavMember.jsx
@@ -5,18 +5,25 @@ import {Link} from "react-router-dom"
 function NavMember() {
   return (
     <nav className={style.navContainer}>
-      <div className={style.logoContainer}>
-        <div className={style.circlesContainer}>
-          <div className={style.circle1}></div>
-          <div className={style.circle2}></div>
+      <div className={style.logoAndSearchContainer}>
+        <div className={style.logoContainer}>
+          <div className={style.circlesContainer}>
+            <div className={style.circle1}></div>
+            <div className={style.circle2}></div>
+          </div>
+          <p className={style.logoName}>EduTutor</p>
         </div>
-        <p className={style.logoName}>EduTutor</p>
-      </div>
-
-      <div className={style.linksContainer}>
-      
+        <div className={style.searchContainer}>
+          <select id="dropdown" name="options" className={style.dropdown}>
+            <option value="" disabled selected hidden className={style.findAJob}>Find Job</option>
+            <option value="tutoring" className={style.tutoring}>Tutoring</option>
+            <option value="class" className={style.class}>Class</option>
+          </select>
+        </div>
         
       </div>
+
+
       <div className={style.logOutButtonContainer}>
         <div className={style.linkContainer}>
           <Link className={style.navLink} to="/">
@@ -28,6 +35,7 @@ function NavMember() {
         </div>
         <button className={style.logOutButton}><Link className={style.logOut}to={"/"}>Log Out</Link></button>
       </div>
+      
     </nav>
   )
 }
